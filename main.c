a47c1d15f6b33824411c98fa9903349cdb73c8ec
Thomas Almeida <99335340+tgo-mas@users.noreply.github.com>
2022-08-26 10:49:32 -0300

    add função infos

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-  
-  printf("SIG-Hat")
-  return 0;
-  
+//////////////////
+///
+///  SIG-Hat
+///
+///  Desenvolvido por: Thomas Almeida
+///  Projeto da disciplina de Programação
+///  Docente: Flavius Gorgônio
+///  Universidade Federal do Rio Grande do Norte
+///
+//////////////////
+
+int main(int argc, char *argv[]) {
+	
+	infos();
+	
+	return 0;
+}
+
+void infos(void){
+	
+    printf("\n");
+    printf("###################################################\n");
+    printf("#                                                 #\n");
+    printf("#        = = = = = S I G - H a t = = = = =        #\n");
+    printf("#                                                 #\n");
+    printf("#   Desenvolvido por: Thomas Almeida			  #\n");
+    printf("#   E-mail: thomas.almeida.109@ufrn.br            #\n");
+    printf("#   Redes sociais: @tgomas.asf                    #\n");
+    printf("#   Git: https://github.com/tgo-mas/SIG-Hat.git   #\n");
+    printf("#                                                 #\n");
+	printf("#   Projeto da disciplina de Programacao		  #\n");
+	printf("#   Docente: Flavius Gorgonio					  #\n");
+	printf("#   Universidade Federal do Rio Grande do Norte   #\n");
+    printf("#                                                 #\n");
+    printf("###################################################\n");
+    printf("\n");
+	
 }
