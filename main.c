72cd72eca331d53604132bc98035c53322b44c84
Thomas Almeida <99335340+tgo-mas@users.noreply.github.com>
2022-08-26 11:08:00 -0300

    Add tela de menu

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 int main(int argc, char *argv[]) {
 	
 	infos();
+	menu();
 	
 	return 0;
 }
@@ -22,20 +23,40 @@ int main(int argc, char *argv[]) {
 void infos(void){
 	
     printf("\n");
-    printf("###################################################\n");
-    printf("#                                                 #\n");
-    printf("#        = = = = = S I G - H a t = = = = =        #\n");
-    printf("#                                                 #\n");
-    printf("#   Desenvolvido por: Thomas Almeida			  #\n");
-    printf("#   E-mail: thomas.almeida.109@ufrn.br            #\n");
-    printf("#   Redes sociais: @tgomas.asf                    #\n");
-    printf("#   Git: https://github.com/tgo-mas/SIG-Hat.git   #\n");
-    printf("#                                                 #\n");
-	printf("#   Projeto da disciplina de Programacao		  #\n");
-	printf("#   Docente: Flavius Gorgonio					  #\n");
-	printf("#   Universidade Federal do Rio Grande do Norte   #\n");
-    printf("#                                                 #\n");
-    printf("###################################################\n");
+    printf("#####################################################\n");
+    printf("##                                                 ##\n");
+    printf("##        = = = = = S I G - H a t = = = = =        ##\n");
+    printf("##                                                 ##\n");
+    printf("##   Desenvolvido por: Thomas Almeida              ##\n");
+    printf("##   E-mail: thomas.almeida.109@ufrn.br            ##\n");
+    printf("##   Redes sociais: @tgomas.asf                    ##\n");
+    printf("##   Git: https://github.com/tgo-mas/SIG-Hat.git   ##\n");
+    printf("##                                                 ##\n");
+	printf("##   Projeto da disciplina de Programacao          ##\n");
+	printf("##   Docente: Flavius Gorgonio                     ##\n");
+	printf("##   Universidade Federal do Rio Grande do Norte   ##\n");
+    printf("##                                                 ##\n");
+    printf("#####################################################\n");
+    printf("\n");
+	
+}
+
+void menu(void){
+	
+    printf("\n");
+    printf("#####################################################\n");
+    printf("##                                                 ##\n");
+    printf("##        = = = = = S I G - H a t = = = = =        ##\n");
+    printf("##                                                 ##\n");
+    printf("##        1 - Catalogo de Bones e Chapeus          ##\n");
+    printf("##            2 - Controle de Estoque              ##\n");
+    printf("##            3 - Painel de Controle               ##\n");
+    printf("##                                                 ##\n");
+    printf("##                                                 ##\n");
+    printf("##       9 - Informacoes sobre o programa          ##\n");
+    printf("##                    0 - Sair                     ##\n");
+    printf("##                                                 ##\n");
+    printf("#####################################################\n");
     printf("\n");
 	
 }
