997a706ab2d51c79c4a2a946f22484d5284a71a0
tgomas <talmeidasf@gmail.com>
2022-11-19 08:48:10 -0300

    Reajuste nos preços dos bones

--- libs/validacoes.c
+++ libs/validacoes.c
@@ -10,10 +10,10 @@ int validaTec(int tecido){
     switch(tecido){
         case 1:
             printf("\n  Tecido selecionado: Poliéster.");
-            return 5;
+            return 3;
         case 2:
             printf("\n  Tecido selecionado: Algodão.");
-            return 7;
+            return 5;
         default:
             printf("\n  Erro: O tecido escolhido não consta nas opções!");
             return 0;
@@ -26,13 +26,13 @@ int validaBord(int bordado){
     switch(bordado){
         case 1:
             printf("\n  Estilo de bordado: Simples.");
-            return 5;
+            return 4;
         case 2:
             printf("\n  Estilo de bordado: Moderado.");
-            return 7;
+            return 5;
         case 3:
             printf("\n  Estilo de bordado: Complexo.");
-            return 9;
+            return 6;
         default: 
             printf("\n  Erro: O estilo selecionado não consta nas opções!");
             return 0;
@@ -45,13 +45,13 @@ int validaMod(int modelo){
    switch(modelo){
         case 1:
             printf("\n  Modelo escolhido: Aba curva.");
-            return 5;
+            return 3;
         case 2:
             printf("\n  Modelo escolhido: Aba reta.");
-            return 6;
+            return 4;
         case 3:
             printf("\n  Modelo escolhido: Polo.");
-            return 7;
+            return 5;
         default:
             printf("\n  Erro: O modelo selecionado não consta nas opções!");
             return 0;
@@ -64,13 +64,13 @@ int validaFecho(int fecho){
    switch(fecho){
         case 1:
             printf("\n  Fecho escolhido: Plástico.");
-            return 8;
+            return 5;
         case 2:
             printf("\n  Fecho escolhido: Velcro.");
-            return 10;
+            return 7;
         case 3:
             printf("\n  Fecho escolhido: Tecido.");
-            return 12;
+            return 9;
         default:
             printf("\n  Erro: O fecho selecionado não consta nas opções!");
             return 0;
